4057fe89e983a81eb57c8ac15ff0bbcc4147804e
anattzlil <addminit@MacBook-Pro.local>
2017-12-20 10:37:39 +0200

    add scroll & 2nd section

---
+++ main.js
@@ -0,0 +1,23 @@
+/*$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+*/
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 2000, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
